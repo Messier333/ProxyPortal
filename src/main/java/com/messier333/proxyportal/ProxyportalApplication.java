02c365771b4051d50b5231edfda1f63c6120d0b7
Messier333 <bdqueen3250@gmail.com>
2025-12-19 20:19:31 +0900

    lombok version change

--- src/main/java/com/messier333/proxyportal/ProxyportalApplication.java
+++ src/main/java/com/messier333/proxyportal/ProxyportalApplication.java
@@ -3,7 +3,6 @@ package com.messier333.proxyportal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
--- src/main/java/com/messier333/proxyportal/user/entity/User.java
+++ src/main/java/com/messier333/proxyportal/user/entity/User.java
@@ -1,10 +1,16 @@
 package com.messier333.proxyportal.user.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @Entity
