013d01268e3c35ee7ad6af24d968645845df38e2
Messier333 <36399805+Messier333@users.noreply.github.com>
2025-12-19 16:27:31 +0900

    Add files via upload

---
+++ src/main/java/com/messier333/proxyportal/ProxyportalApplication.java
@@ -0,0 +1,13 @@
+package com.messier333.proxyportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/messier333/proxyportal/ProxyportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messier333.proxyportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
